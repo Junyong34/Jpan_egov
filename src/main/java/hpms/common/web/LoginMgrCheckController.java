8e236d9cdd51c79f3d2790622808d8785739762e
PARK-PC\Administrator <wnsdyd21@gmail.com>
2017-02-07 09:26:12 +0900

    update

--- src/main/java/hpms/common/web/LoginMgrCheckController.java
+++ src/main/java/hpms/common/web/LoginMgrCheckController.java
@@ -1,5 +1,5 @@
-
 package hpms.common.web;
+
 import java.util.*;
 import java.io.*;
 
@@ -27,114 +27,105 @@ import egovframework.rte.ptl.mvc.tags.ui.pagination.PaginationInfo;
 import org.springframework.web.servlet.ModelAndView;
 
 import hpms.common.service.LoginMgrCheck;
+
 @Controller
-public class LoginMgrCheckController
-{
-    @Autowired
-    private LoginMgrCheck LoginMgrCheckService;
-    @RequestMapping(value="/2016041114474LoginMgrCheck_System_Developer_Check.do")
-    public ModelAndView System_Developer_Check(ModelMap model, DOBJ idobj, HttpServletRequest request) throws Exception
-    {
+public class LoginMgrCheckController {
+	@Autowired
+	private LoginMgrCheck LoginMgrCheckService;
 
-        ModelAndView mav = new ModelAndView("riaView");
-        
-    
-        DOBJ odobj = LoginMgrCheckService.System_Developer_Check(idobj,request);
-        mav.addObject("WIZDOBJ", odobj);
-        mav.addObject("SEL3", odobj.getRetObject("SEL3").getRecords());
-        mav.addObject("G", odobj.getRetObject("G").getRecordMap());
-        mav.addObject("MESSAGE", odobj.getRetmsg());
-        return mav;
-    }
-    @RequestMapping(value="/2016041114474LoginMgrCheck_wizLoginSession.do")
-    public ModelAndView wizLoginSession(ModelMap model, DOBJ idobj, HttpServletRequest request) throws Exception
-    {
+	@RequestMapping(value = "/2016041114474LoginMgrCheck_System_Developer_Check.do")
+	public ModelAndView System_Developer_Check(ModelMap model, DOBJ idobj,
+			HttpServletRequest request) throws Exception {
 
-        ModelAndView mav = new ModelAndView("riaView");
-        
-    
-        DOBJ odobj = LoginMgrCheckService.wizLoginSession(idobj,request);
-        mav.addObject("WIZDOBJ", odobj);
-        mav.addObject("SEL3", odobj.getRetObject("SEL3").getRecords());
-        mav.addObject("SEL1", odobj.getRetObject("SEL1").getRecords());
-        mav.addObject("G", odobj.getRetObject("G").getRecordMap());
-        mav.addObject("MESSAGE", odobj.getRetmsg());
-        return mav;
-    }
-    
-    
-    @RequestMapping(value="/2016041114474LoginMgrCheck_SessionLogInfo.do")
-    public String SessionLogInfo(ModelMap model, DOBJ idobj ,HttpServletRequest request) throws Exception
-    {
-    
-        DOBJ odobj = LoginMgrCheckService.SessionLogInfo(idobj,request);
-        
-        model.addAttribute("WIZDOBJ", odobj);
-        model.addAttribute("SEL1", odobj.getRetObject("SEL1").getRecords());
-        model.addAttribute("G", odobj.getRetObject("G").getRecordMap());
-        model.addAttribute("MESSAGE", odobj.getRetmsg());
-        
-        VOBJ vobj = new VOBJ();
-        
+		ModelAndView mav = new ModelAndView("riaView");
 
-        vobj = odobj.getRetObject("SEL1");
-        ArrayList _clist = vobj.getColumnNames();
-        int LoginColCnt = vobj.getColumnCnt();
+		DOBJ odobj = LoginMgrCheckService
+				.System_Developer_Check(idobj, request);
+		mav.addObject("WIZDOBJ", odobj);
+		mav.addObject("SEL3", odobj.getRetObject("SEL3").getRecords());
+		mav.addObject("G", odobj.getRetObject("G").getRecordMap());
+		mav.addObject("MESSAGE", odobj.getRetmsg());
+		return mav;
+	}
 
-        HashMap loginInfo = new HashMap();
-        HttpSession session = request.getSession(true);
-      
-    
-        if(_clist.size() == 0  ) return "forward:/2016041114474Main.do";
-        
-       
-        session.setMaxInactiveInterval(1800);
+	@RequestMapping(value = "/2016041114474LoginMgrCheck_wizLoginSession.do")
+	public ModelAndView wizLoginSession(ModelMap model, DOBJ idobj,
+			HttpServletRequest request) throws Exception {
 
-        for (int i = 0; i < _clist.size(); i++)
-        {
-          loginInfo.put(_clist.get(i), vobj.getRecord().get(_clist.get(i)));
-        }
-       // #########세션 생성
-        session.setAttribute("G", loginInfo); 
-     
-        return "forward:/2016041114474Main.do";  
-        
-      
-    }
-    
-    @RequestMapping({"/2016041114474Main.do"})
-    public String MainPage(ModelMap model, DOBJ idobj, HttpServletRequest request)
-      throws Exception
-    {
-  	  HttpSession session = request.getSession(true);
-  	  HashMap hmap = null;
-  	  hmap =  (HashMap) session.getAttribute("G");
-  	  
-  	
-  	  if  (( session == null) || session.getAttribute("G") == null || hmap.size() == 0){
-  	  	  System.out.println(" NOT SESSION.");
-  	  	  // 로그인페이지로 돌린다
-  	  	 return "redirect:/";
-       
-  	  }else{  
-  		  
-  		model.addAttribute("G", hmap);
-  	  	// 세션정보가 살아있으면 메인페이지로 이동
-  	  	return "/jsp/menu/Main_menu";
-  	  	
-  	  }
-  	  
-    
-    }
-    
-    @RequestMapping({"/2016041114474loggout.do"})
-    public String LogoutPage(ModelMap model, DOBJ idobj, HttpServletRequest request)
-      throws Exception
-    {
-    	
-    	
-  	  	return "/jsp/menu/LogOut";
-  	  	
+		ModelAndView mav = new ModelAndView("riaView");
 
-    }
+		DOBJ odobj = LoginMgrCheckService.wizLoginSession(idobj, request);
+		mav.addObject("WIZDOBJ", odobj);
+		mav.addObject("SEL3", odobj.getRetObject("SEL3").getRecords());
+		mav.addObject("SEL1", odobj.getRetObject("SEL1").getRecords());
+		mav.addObject("G", odobj.getRetObject("G").getRecordMap());
+		mav.addObject("MESSAGE", odobj.getRetmsg());
+		return mav;
+	}
+
+	@RequestMapping(value = "/2016041114474LoginMgrCheck_SessionLogInfo.do")
+	public String SessionLogInfo(ModelMap model, DOBJ idobj,
+			HttpServletRequest request) throws Exception {
+
+		DOBJ odobj = LoginMgrCheckService.SessionLogInfo(idobj, request);
+
+		model.addAttribute("WIZDOBJ", odobj);
+		model.addAttribute("SEL1", odobj.getRetObject("SEL1").getRecords());
+		model.addAttribute("G", odobj.getRetObject("G").getRecordMap());
+		model.addAttribute("MESSAGE", odobj.getRetmsg());
+
+		VOBJ vobj = new VOBJ();
+
+		vobj = odobj.getRetObject("SEL1");
+		ArrayList _clist = vobj.getColumnNames();
+		int LoginColCnt = vobj.getColumnCnt();
+
+		HashMap loginInfo = new HashMap();
+		HttpSession session = request.getSession(true);
+
+		if (_clist.size() == 0)
+			return "forward:/2016041114474Main.do";
+
+		session.setMaxInactiveInterval(360000);
+
+		for (int i = 0; i < _clist.size(); i++) {
+			loginInfo.put(_clist.get(i), vobj.getRecord().get(_clist.get(i)));
+		}
+		// #########세션 생성
+		session.setAttribute("G", loginInfo);
+
+		return "forward:/2016041114474Main.do";
+
+	}
+
+	@RequestMapping({ "/2016041114474Main.do" })
+	public String MainPage(ModelMap model, DOBJ idobj,
+			HttpServletRequest request) throws Exception {
+		HttpSession session = request.getSession(true);
+		HashMap hmap = null;
+		hmap = (HashMap) session.getAttribute("G");
+
+		if ((session == null) || session.getAttribute("G") == null
+				|| hmap.size() == 0) {
+			System.out.println(" NOT SESSION.");
+			// 로그인페이지로 돌린다
+			return "redirect:/";
+
+		} else {
+
+			model.addAttribute("G", hmap);
+			// 세션정보가 살아있으면 메인페이지로 이동
+			return "/jsp/menu/Main_menu";
+
+		}
+
+	}
+
+	@RequestMapping({ "/2016041114474loggout.do" })
+	public String LogoutPage(ModelMap model, DOBJ idobj,
+			HttpServletRequest request) throws Exception {
+
+		return "/jsp/menu/LogOut";
+
+	}
 }
